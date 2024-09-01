7259e9252abe5e1aadaddd45f6c34e5a0d822f21
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-09-01 14:08:51 -0500

    Added response chat

---
+++ yes_no_app/lib/presentation/widgets/chat/her_messages_bubble.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key, required this.message});
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        "https://yesno.wtf/assets/no/28-e19b6f658f621f7c5980a33f8249a65d.gif",
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}

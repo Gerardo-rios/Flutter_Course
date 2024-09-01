90f7c8bb37d3cfd93daf006ecee4c6f96a0504be
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-09-01 12:56:18 -0500

    Added list view

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -6,14 +6,43 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/08191f3a-35e9-4d24-84d6-b9796714f798/d6e3g88-6c6667f2-9b55-4c72-9136-5bfebb53d9fe.png/v1/fill/w_256,h_256/chloe_grace_moretz_tg_by_dartmuth_d6e3g88-fullview.png"),
-          )),
-      title: const Text('My Love'),
-    ));
+      appBar: AppBar(
+        leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/08191f3a-35e9-4d24-84d6-b9796714f798/d6e3g88-6c6667f2-9b55-4c72-9136-5bfebb53d9fe.png/v1/fill/w_256,h_256/chloe_grace_moretz_tg_by_dartmuth_d6e3g88-fullview.png"),
+            )),
+        title: const Text('My Love'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Message $index'),
+                  );
+                },
+              ),
+            ),
+            const Divider(height: 1),
+            Text('data'),
+          ],
+        ),
+      ),
+    );
   }
 }
